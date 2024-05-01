08b5d7f60893ba6831ff180fb097d65f3c67cab0
Melkid69 <167970274+Melkid45@users.noreply.github.com>
2024-05-01 11:34:44 +0600

    Last build

---
+++ app/js/home.js
@@ -0,0 +1,48 @@
+var splide = new Splide( '#splide1', {
+    type   : 'loop',
+    gap: '32px',
+    pagination: false
+} );
+
+splide.mount();
+var splide = new Splide( '#splide2', {
+    type   : 'loop',
+    gap: '32px',
+    pagination: false
+} );
+
+splide.mount();
+
+
+
+$.fn.setCursorPosition = function (pos) {
+    if ($(this).get(0).setSelectionRange) {
+    $(this).get(0).setSelectionRange(pos, pos);
+    } else if ($(this).get(0).createTextRange) {
+    var range = $(this).get(0).createTextRange();
+    range.collapse(true);
+    range.moveEnd("character", pos);
+    range.moveStart("character", pos);
+    range.select();
+    }
+};
+
+
+$("#tel").click(function(){
+    $(this).setCursorPosition(3);
+}).mask("+7(999) 999-9999");
+$("#tel2").click(function(){
+    $(this).setCursorPosition(3);
+}).mask("+7(999) 999-9999");
+
+
+document.oninput = function() {
+    var input = document.querySelector('.weight');
+    var input2 = document.querySelector('.status__body-item-inp');
+    input.value = input.value.replace (/\D/g, '');
+    input2.value = input2.value.replace (/\D/g, '');
+}
+
+function replacer(el) {
+    el.value = el.value.replace(/[^0-9Xx]/g, ''); 
+}
--- app/js/main.js
+++ app/js/main.js
@@ -1,63 +1,68 @@
 function onEntry(entry) {
-    entry.forEach(change => {
+  entry.forEach((change) => {
     if (change.isIntersecting) {
-    change.target.classList.add('element-show');
+      change.target.classList.add("element-show");
     }
-    });
+  });
 }
 
-let options = {threshold: [0.5] };
+let options = { threshold: [0.5] };
 let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.element-animation');
+let elements = document.querySelectorAll(".element-animation");
 
 for (let elm of elements) {
-    observer.observe(elm);
+  observer.observe(elm);
 }
-
-
 
 ymaps.ready(init);
 
-
 function init() {
-    var myMap = new ymaps.Map("map__info", {
-        center: [55.78150702329822, 37.58069404049517],
-        zoom: 13,
-        controls: [
-            'zoomControl', 
-            'typeSelector',
-            'searchControl'
-        ]
-    });
-    // Собственное изображение для метки с контентом
-    var placemark4 = new ymaps.Placemark([55.78150702329822, 37.58069404049517], {
-        hintContent: 'Москва, Art Residence,3-я Ямского Поля ул., 9к4, офис Е316',
-    }, {
-        // Опции.
- 
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
- 
-        // Своё изображение иконки метки.
-        iconImageHref: "https://rgcompany.ru/images/tild3431-3737-4562-a163-643934666561__holder.svg"
-        ,
-        // Размеры метки.
-        iconImageSize: [48, 48],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-16, -16],
-    });
-    jQuery(function($){
-    $(document).mouseup(function(e){
-        if (!$('.maps').is(e.target) && $('.maps').has(e.target).length === 0){
-            myMap.behaviors.disable('scrollZoom');
-            $('.body').css('overflow', 'hidden');
-        }else{
-            myMap.behaviors.enable('scrollZoom');
+  var myMap = new ymaps.Map("map__info", {
+    center: [55.78150702329822, 37.58069404049517],
+    zoom: 13,
+    controls: ["zoomControl", "typeSelector", "searchControl"],
+  });
+  // Собственное изображение для метки с контентом
+  var placemark4 = new ymaps.Placemark(
+    [55.78150702329822, 37.58069404049517],
+    {
+      hintContent: "Москва, Art Residence,3-я Ямского Поля ул., 9к4, офис Е316",
+    },
+    {
+      // Опции.
 
-        }
-    })
-})
-    myMap.geoObjects.add(placemark4);
+      // Необходимо указать данный тип макета.
+      iconLayout: "default#image",
 
+      // Своё изображение иконки метки.
+      iconImageHref:
+        "https://rgcompany.ru/images/tild3431-3737-4562-a163-643934666561__holder.svg",
+      // Размеры метки.
+      iconImageSize: [48, 48],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-16, -16],
+    }
+  );
+//   jQuery(function ($) {
+//     $(document).mouseup(function (e) {
+//       if (!$(".maps").is(e.target) && $(".maps").has(e.target).length === 0) {
+//         myMap.behaviors.disable("scrollZoom");
+//         $(".body").css("overflow", "hidden");
+//       } else {
+//         myMap.behaviors.enable("scrollZoom");
+//       }
+//     });
+//   });
+  myMap.geoObjects.add(placemark4);
 }
+
+let mapInfoWidth = 568
+let mapInfoHight = 341
+$(document).on('load' , function(){
+    $('#map__info').css({
+        "width" : `${mapInfoWidth} + px`,
+        "height" : `${mapInfoHight} + px`
+    }
+    )
+})
